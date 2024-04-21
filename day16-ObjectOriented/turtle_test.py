887561ad2f435e6bfc49e9616cb9741117ad8d3a
Sherif Mohamed <sherif425@gmail.com>
2024-04-21 17:42:39 +0200

    turtle module

---
+++ day16-ObjectOriented/turtle_test.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+from random import random
+
+t = Turtle()
+# for i in range(100):
+#     steps = int(random() * 100)
+#     angle = int(random() * 360)
+#     t.right(angle)
+#     t.fd(steps)
+
+
+
+
+
+
+
+myScreen = Screen()
+myScreen.exitonclick()
